8f24a2447f0a7e3c3ee6a4248f753dadc2f15d7a
urvish <urvishpvt@gmail.com>
2022-12-04 22:55:49 +0530

    ss

---
+++ build/_next/static/cmNt7kIhuxK8pP0lbooiK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6c18d5351bdb4643.css","static/chunks/pages/index-987f4cac4c6b28e0.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
